79ae0044317bc320b7cf1d794f8fcf4a4fdff5ee
TimeDA <116543756+TimeDA@users.noreply.github.com>
2022-11-29 17:16:16 +0800

    Update transform_10.py

--- src/Generating Negative Samples/get_transform/transform_10.py
+++ src/Generating Negative Samples/get_transform/transform_10.py
@@ -14,14 +14,12 @@ def transform(prog_fullpath, target_author, orig_prog_path, save_to, ignore_list
     authors_root = os.path.dirname(target_author)
     randnum = random.randint(0, len(os.listdir(authors_root)))
     if randnum == 0:
-        #10.1转10.2
         e = typedef.init_parse(prog_fullpath)
         flag, doc, new_ignore_list = typedef.trans_define(e, ignore_list)
         if flag:
             per_tf_ignore_list = new_ignore_list
             typedef.save_file(doc, save_to)
     else:
-        #10.2转10.1
         flag, doc, new_ignore_list = retypedef.program_transform(prog_fullpath, target_author, ignore_list)
         if flag:
             per_tf_ignore_list = new_ignore_list

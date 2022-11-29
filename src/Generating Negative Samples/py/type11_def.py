df0e2f9a4c8ec17d81b01603752e4c362a728836
TimeDA <116543756+TimeDA@users.noreply.github.com>
2022-11-29 17:49:17 +0800

    Update type11_def.py

--- src/Generating Negative Samples/py/type11_def.py
+++ src/Generating Negative Samples/py/type11_def.py
@@ -4,8 +4,8 @@ import glob
 
 from lxml.etree import Element
 
-path='.\\projects'#特征作者提取xml文件库
-d_path='.\\dest'#替换目标作者xml文件库
+path='.\\projects'
+d_path='.\\dest'
 
 from lxml import etree
 doc=None
@@ -13,9 +13,9 @@ ns = {'src': 'http://www.srcML.org/srcML/src',
    'cpp': 'http://www.srcML.org/srcML/cpp',
    'pos': 'http://www.srcML.org/srcML/position'}
 
-ls=[] #元素为列表[1,2,3] 1宏定义名出现次数 2宏定义名 3宏定义值（不同值只选第一个）
+ls=[] 
 
-def_Min=2 #设定宏定义阈值
+def_Min=2
 
 def init_parse(file):
     global doc
@@ -39,10 +39,8 @@ def save_file(doc, f):
         r.write(etree.tostring(doc).decode('utf-8'))
 
 def trans_define(e,l):
-    #获得所有的宏定义标签
     defines=get_defines(e)
     all_names=get_allvarname(e)
-    #首先排除已存在的相同define标签
     for name_var in all_names:
         if len(name_var) != 0: continue
         if name_var.text==l[1]:
@@ -51,12 +49,6 @@ def trans_define(e,l):
         if define[1][0].text==l[1]:
             return
 
-    #获得所有的name标签 只要name和宏定义的0name相同则替换宏定义中的值
-    # names=get_allname(e)
-    # for name in names:
-    #     if name.text==l[2]:
-    #         name.text=l[1]
-    #将l作为宏定义加到该文件中
     new_define=Element('define')
     new_define.text='#define '+str(l[1])+' '+str(l[2])+'\n'
 
@@ -71,20 +63,18 @@ def trans_define(e,l):
             elem.getparent().insert(des+1,new_define)
         else:
             des=0
-            print("文件既没有using也没有include")
             elem=e('//src:unit')[0]
             if elem.getparent() is not None:
                 elem.getparent().insert(des+1,new_define)
 
 def creat_def_list(e):
     global ls
-    #获取define标签
     defines=get_defines(e)
     flag=0
     for define in defines:
         if len(define) == 3 and len(define[1]) == 1:
             for l in ls:
-                if define[1][0].text==l[1]:  #找到有该宏定义名
+                if define[1][0].text==l[1]:
                     l[0]+=1
                     flag=1
                     break
@@ -95,9 +85,7 @@ def program_transform(program_path,author_path,ignore_list=[]):
     global ls
     files =[f for f in glob.glob(author_path+"**/*.xml",recursive=True)]
     for f in files:
-        #对一个文件列表循环解析
         e = init_parse(f)
-        #每循环一个文件记录其中的宏定义
         creat_def_list(e)
     e = init_parse(program_path)
     global flag
@@ -113,17 +101,7 @@ def program_transform(program_path,author_path,ignore_list=[]):
         trans_define(e, l)
         flag=True
         value_name = l[1]
-        # 记录define名字
         new_ignore_list.append(value_name)
     save_file(doc, './style/style.xml')
     ls=[]
     return flag,tree_root,new_ignore_list
-
-    #取超过阈值的宏定义  新增到目标作者的源代码中
-    # des=[f for f in glob.glob(d_path+'**/*.xml',recursive=True)]
-    # for f in des:
-    #     e=init_parse(f)
-    #     for l in ls:
-    #         if l[0]>=def_Min:
-    #             trans_define(e,l,f)
-
